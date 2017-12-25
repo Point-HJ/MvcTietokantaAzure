51f4b1e1d102b0246fc24c7a84b5c6d7f3220814
Heidi Joenpolvi <heidi.joenpolvi@stud.pointcollege.fi>
2017-12-25 12:03:30 +0200

    HenkilotController + Indexin luonti

---
+++ MvcTietokantaAzure/MvcTietokantaAzure/Controllers/HenkilotController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTietokantaAzure.Controllers
+{
+    public class HenkilotController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
