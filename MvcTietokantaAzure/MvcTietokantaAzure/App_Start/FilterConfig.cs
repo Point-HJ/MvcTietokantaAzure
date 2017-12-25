7098b0e2ba507b7d5c7c8a3a254adc5e9e0ee3b0
Heidi Joenpolvi <heidi.joenpolvi@stud.pointcollege.fi>
2017-12-25 11:48:59 +0200

    Uuden projektin luonti. Ei koodattu vielä mitään.

---
+++ MvcTietokantaAzure/MvcTietokantaAzure/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTietokantaAzure
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MvcTietokantaAzure/MvcTietokantaAzure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcTietokantaAzure.MvcApplication" Language="C#" %>
