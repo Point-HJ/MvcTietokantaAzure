a6c3fe25b8275289f38c99f1d4a2c29c2df16eb0
Heidi Joenpolvi <heidi.joenpolvi@stud.pointcollege.fi>
2017-12-26 12:47:02 +0200

    Muokkaustoiminnon lisääminen, Henkilö-ID ei vielä näy muokkausikkunassa, pitää muuttaa stringiksi. Tallennus ei vielä toimi.

--- MvcTietokantaAzure/MvcTietokantaAzure/Controllers/HenkilotController.cs
+++ MvcTietokantaAzure/MvcTietokantaAzure/Controllers/HenkilotController.cs
@@ -13,7 +13,7 @@ namespace MvcTietokantaAzure.Controllers
         // GET: Henkilot
         public ActionResult Index()
         {
-            ViewBag.OmaTieto = "Höpölöpö";
+            ViewBag.OmaTieto = "Testi";
 
             KoulukantaEntities entities = new KoulukantaEntities();
             List<HENKILOT> model = entities.HENKILOT.ToList();
@@ -28,12 +28,16 @@ namespace MvcTietokantaAzure.Controllers
             return View();
         }
 
+        public ActionResult Index3()
+        {
+
+            return View();
+        }
 
         public JsonResult GetList()
         {
             KoulukantaEntities entities = new KoulukantaEntities();
-            // List<HENKILOT> model = entities.HENKILOT.ToList();
-
+           
             var model = (from h in entities.HENKILOT
                          select new
                          { 
@@ -50,6 +54,31 @@ namespace MvcTietokantaAzure.Controllers
 
           //  Response.Expires = -1;
            // Response.CacheControl = "no-cache";
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetSingleHenkilo(int id)
+        {
+            KoulukantaEntities entities = new KoulukantaEntities();
+            string s = id.ToString();
+            var model = (from h in entities.HENKILOT
+                         where h.HenkiloID == id
+                         select new
+                         {
+                             HenkiloID = h.HenkiloID,
+                             Etunimi = h.Etunimi,
+                             Sukunimi = h.Sukunimi,
+                             Osoite = h.Osoite,
+                             Esimies = h.Esimies
+                         }).FirstOrDefault();
+
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+
+            //  Response.Expires = -1;
+            // Response.CacheControl = "no-cache";
 
             return Json(json, JsonRequestBehavior.AllowGet);
         }
