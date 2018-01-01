a21ecc8c2803c5b37b6b8b5f029e355c7bd694c5
Heidi Joenpolvi <heidi.joenpolvi@stud.pointcollege.fi>
2018-01-01 18:11:02 +0200

    Tietojen poisto ja uuden henkilön lisäys, Henkilö-ID ei vielä näy muokkausikkunassa, pitää muuttaa stringiksi. Tallennus ei vielä toimi.

--- MvcTietokantaAzure/MvcTietokantaAzure/Controllers/HenkilotController.cs
+++ MvcTietokantaAzure/MvcTietokantaAzure/Controllers/HenkilotController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 namespace MvcTietokantaAzure.Controllers
 {
     public class HenkilotController : Controller
+       
     {
         // GET: Henkilot
         public ActionResult Index()
@@ -52,16 +53,16 @@ namespace MvcTietokantaAzure.Controllers
             string json = JsonConvert.SerializeObject(model);
             entities.Dispose();
 
-          //  Response.Expires = -1;
-           // Response.CacheControl = "no-cache";
+          Response.Expires = -1;
+          Response.CacheControl = "no-cache";
 
             return Json(json, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult GetSingleHenkilo(int id)
+        public JsonResult GetSingleHenkilo(string id) //Pitää muuttaa stringiksi!?
         {
             KoulukantaEntities entities = new KoulukantaEntities();
-            string s = id.ToString();
+
             var model = (from h in entities.HENKILOT
                          where h.HenkiloID == id
                          select new
@@ -71,17 +72,90 @@ namespace MvcTietokantaAzure.Controllers
                              Sukunimi = h.Sukunimi,
                              Osoite = h.Osoite,
                              Esimies = h.Esimies
-                         }).FirstOrDefault();
+                         }
+                        
+                         ).FirstOrDefault();
+            
+       
 
 
             string json = JsonConvert.SerializeObject(model);
             entities.Dispose();
 
-            //  Response.Expires = -1;
-            // Response.CacheControl = "no-cache";
-
             return Json(json, JsonRequestBehavior.AllowGet);
         }
+
+
+        public ActionResult Update(HENKILOT henk)
+        {
+            KoulukantaEntities entities = new KoulukantaEntities();
+            string id = henk.HenkiloID;
+
+            bool OK = false;
+
+            // onko kyseessä muokkaus vai uuden lisääminen?
+            if (id == "(uusi)")
+            {
+                // kyseessä on uuden asiakkaan lisääminen, kopioidaan kentät
+                HENKILOT dbItem = new HENKILOT()
+                {
+                    HenkiloID = henk.HenkiloID,
+                    Etunimi = henk.Etunimi,
+                    Sukunimi = henk.Sukunimi,
+                    Osoite = henk.Osoite,
+                    Esimies = henk.Esimies
+                };
+
+                // tallennus tietokantaan
+                entities.HENKILOT.Add(dbItem);
+                entities.SaveChanges();
+                OK = true;
+            }
+            else
+            {
+                // muokkaus, haetaan id:n perusteella riviä tietokannasta
+                HENKILOT dbItem = (from c in entities.HENKILOT
+                                   where c.HenkiloID == id
+                                   select c).FirstOrDefault();
+                if (dbItem != null)
+                {
+                    dbItem.Etunimi = henk.Etunimi;
+                    dbItem.Sukunimi = henk.Sukunimi;
+                    dbItem.Osoite = henk.Osoite;
+                    dbItem.Esimies = henk.Esimies;
+
+
+                    // tallennus tietokantaan
+                    entities.SaveChanges();
+                    OK = true;
+                }
+            }
+            entities.Dispose();
+
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            KoulukantaEntities entities = new KoulukantaEntities();
+
+            // etsitään id:n perusteella asiakasrivi kannasta
+            bool OK = false;
+            HENKILOT dbItem = (from h in entities.HENKILOT
+                               where h.HenkiloID == id
+                               select h).FirstOrDefault();
+            if (dbItem != null)
+            {
+                // tietokannasta poisto
+                entities.HENKILOT.Remove(dbItem);
+                entities.SaveChanges();
+                OK = true;
+            }
+            entities.Dispose();
+
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+
     }
        
 }
